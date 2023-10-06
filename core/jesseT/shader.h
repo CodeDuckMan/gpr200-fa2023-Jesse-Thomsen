e85630ed92f9038f9dde801218d1df608ce24794
Jesse <jesse.thomsen@mymail.champlain.edu>
2023-10-06 01:58:15 -0400

    Updated A-3 to step just before Texture Properties
    Created transformations.h
    Added all functions to transformations.h up to Transform struct.

--- core/jesseT/shader.h
+++ core/jesseT/shader.h
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <fstream>
 #include "../ew/external/glad.h"
+#include "../ew/ewMath/ewMath.h"
 
 namespace jesseT {
 	std::string loadShaderSourceFromFile(const std::string& filePath);
@@ -19,6 +20,8 @@ namespace jesseT {
 		void setVec2(const std::string& name, float x, float y) const;
 		void setVec3(const std::string& name, float x, float y, float z) const;
 		void setVec4(const std::string& name, float x, float y, float z, float w) const;
+
+
 
 	private:
 		unsigned int m_id;
---
+++ core/jesseT/texture.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace jesseT {
+	unsigned int loadTexture(const char* filePath);
+}
